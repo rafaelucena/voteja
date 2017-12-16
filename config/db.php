7a9a37b014a2fefc659681e95f9544fd9f41cb1a
rafaelucena <rafael.boszko@gmail.com>
2017-12-16 22:42:17 +0100

    #0001 - Decided on using on DB only tables currently in USE.

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'dsn' => 'sqlite:@app/database/voteja.sqlite',
+    'dsn' => 'sqlite:@app/database/voteja.db',
 //    'username' => '',
 //    'password' => '',
 //    'charset' => '',
