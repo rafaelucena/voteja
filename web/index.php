92cb25a8d57cf8d18b88e821251fa6344796c60e
rafaelucena <rafael.boszko@gmail.com>
2017-12-27 00:36:29 +0100

    #0015 (add) - Create component/helper structure (Adding custom helper to configuration file)

--- web/index.php
+++ web/index.php
@@ -7,6 +7,9 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
+// Custom helpers
+Yii::$classMap['yii\helpers\Common'] = '@app/components/Common.php';
+
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
