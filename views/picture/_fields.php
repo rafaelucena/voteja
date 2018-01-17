93c3c4260412f2e4ee3d1d781aade9d20b538bb0
rafaelucena <rafael.boszko@gmail.com>
2018-01-17 08:13:45 +0100

    #0043 (fix) - Transform attribute -picture_type_id from *picture into select

--- views/picture/_fields.php
+++ views/picture/_fields.php
@@ -1,4 +1,9 @@
 <?php
+
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use app\models\PictureType;
+
 /* @var $this yii\web\View */
 /* @var $model app\models\Picture */
 /* @var $form yii\widgets\ActiveForm */
@@ -9,6 +14,8 @@
     <?= $form->field($model, 'image')->fileInput() ?>
 
     <?= $form->field($model, 'picture_type_id')->textInput() ?>
+    <?= Html::activeDropDownList($model, 'id',
+        ArrayHelper::map(PictureType::find()->all(), 'id', 'name')) ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
