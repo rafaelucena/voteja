f5ddfaeae5e46e6bbb039407504949ec4cad930c
rafaelucena <rafael.boszko@gmail.com>
2018-02-04 23:55:31 +0100

    #0063 (#0045) (add) - New file structure for *source data (Adapting changes from database)

--- models/Source.php
+++ models/Source.php
@@ -8,6 +8,7 @@ use Yii;
  * This is the model class for table "source".
  *
  * @property int $id
+ * @property int $source_valid_id
  * @property string $title
  * @property string $description
  * @property string $url
@@ -15,13 +16,16 @@ use Yii;
  * @property bool $active
  * @property int $created_by
  * @property int $updated_by
+ * @property int $deleted_by
  * @property string $created
  * @property string $updated
  * @property string $deleted
  *
  * @property PartySource[] $partySources
- * @property User $updatedBy
+ * @property SourceValid $sourceValid
  * @property User $createdBy
+ * @property User $updatedBy
+ * @property User $deletedBy
  */
 class Source extends \yii\db\ActiveRecord
 {
@@ -39,13 +43,15 @@ class Source extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
+            [['source_valid_id'/*, 'created_by', 'created'*/], 'required'],
+            [['source_valid_id', 'created_by', 'updated_by', 'deleted_by'], 'integer'],
             [['when', 'created', 'updated', 'deleted'], 'safe'],
             [['active'], 'boolean'],
-            [['created_by', 'created'], 'required'],
-            [['created_by', 'updated_by'], 'integer'],
             [['title'], 'string', 'max' => 63],
             [['description'], 'string', 'max' => 127],
             [['url'], 'string', 'max' => 511],
+            [['deleted_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['deleted_by' => 'id']],
+            [['source_valid_id'], 'exist', 'skipOnError' => true, 'targetClass' => SourceValid::className(), 'targetAttribute' => ['source_valid_id' => 'id']],
             [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['updated_by' => 'id']],
             [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
         ];
@@ -58,6 +64,7 @@ class Source extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
+            'source_valid_id' => 'Source Valid ID',
             'title' => 'Title',
             'description' => 'Description',
             'url' => 'Url',
@@ -65,6 +72,7 @@ class Source extends \yii\db\ActiveRecord
             'active' => 'Active',
             'created_by' => 'Created By',
             'updated_by' => 'Updated By',
+            'deleted_by' => 'Deleted By',
             'created' => 'Created',
             'updated' => 'Updated',
             'deleted' => 'Deleted',
@@ -82,9 +90,9 @@ class Source extends \yii\db\ActiveRecord
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getUpdatedBy()
+    public function getSourceValid()
     {
-        return $this->hasOne(User::className(), ['id' => 'updated_by']);
+        return $this->hasOne(SourceValid::className(), ['id' => 'source_valid_id']);
     }
 
     /**
@@ -93,5 +101,21 @@ class Source extends \yii\db\ActiveRecord
     public function getCreatedBy()
     {
         return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUpdatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'updated_by']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDeletedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'deleted_by']);
     }
 }
