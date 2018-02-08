f89443d18983aa17a9409b31998becf55237c553
rafaelucena <rafael.boszko@gmail.com>
2018-02-08 08:10:48 +0100

    #0074 (#0045) (fix) - Adapt unnecessary fields from @form for *sourceKnown and implement field.dropDownList from *trust

--- views/source-known/_fields.php
+++ views/source-known/_fields.php
@@ -1,6 +1,7 @@
 <?php
 
-use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Trust;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\SourceKnown */
@@ -9,18 +10,20 @@ use yii\widgets\ActiveForm;
 
 <div class="source-known-form-fields">
 
-    <?= $form->field($model, 'trust_id')->textInput() ?>
+    <?= $form->field($model, 'trust_id')->label('Trust')->dropDownList(
+        ArrayHelper::map(
+            Trust::find()->all(),
+            'id',
+            function($model) {
+                return $model['level'] . ' - ' . $model['name'];
+            }
+        )
+    ) ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
+
     <?= $form->field($model, 'active')->checkbox() ?>
-
-    <?= $form->field($model, 'created_by')->textInput() ?>
-
-    <?= $form->field($model, 'updated_by')->textInput() ?>
-
-    <?= $form->field($model, 'created')->textInput() ?>
-
-    <?= $form->field($model, 'updated')->textInput() ?>
 
 </div>
